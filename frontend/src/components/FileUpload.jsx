25c60c7661186c0711e900889095377f626f6c4b
Bahadır Aksakal <72493865+bahadraksakal@users.noreply.github.com>
2025-01-13 16:35:53 +0300

    Import centralized URL variable in FileUpload component

--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUpload } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import { url } from "../utils";
 
 library.add(faUpload);
 
